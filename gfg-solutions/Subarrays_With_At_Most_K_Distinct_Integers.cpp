cf46e600bff7e5758a2baa95d7cd2810f8566f9c
Bhhvshh <149655632+Bhhvshh@users.noreply.github.com>
2025-07-26 00:21:53 +0530

    Add GFG solution: Subarrays_With_At_Most_K_Distinct_Integers (2025-07-25)

--- gfg-solutions/Subarrays_With_At_Most_K_Distinct_Integers.cpp
+++ gfg-solutions/Subarrays_With_At_Most_K_Distinct_Integers.cpp
@@ -1,3 +1,15 @@
+// Problem: Subarrays_With_At_Most_K_Distinct_Integers
+// Description: You are given an array arr[] of positive integers and an integer k, find the number of subarrays in arr[] where the count of distinct integers is at most k. Note: A subarray is a contiguous part of an array. Examples: Input: arr[] = [1, 2, 2, 3], k = 2Output: 9Explanation: Subarrays with at most 2 distinct elements are: [1], [2], [2], [3], [1, 2], [2, 2], [2, 3], [1, 2, 2] and [2, 2, 3].
+Input: arr[] = [1, 1, 1], k = 1Output: 6
+Explanation: Subarrays with at most 1 distinct element are: [1], [1], [1], [1, 1], [1, 1] and [1, 1, 1].
+Input: arr[] = [1, 2, 1, 1, 3, 3, 4, 2, 1], k = 2Output: 24
+Explanation: There are 24 subarrays with at most 2 distinct elements.
+Constraints:
+1 ≤ arr.size() ≤ 2*104
+1 ≤ k ≤ 2*104
+1 ≤ arr[i] ≤ 109
+// GeeksforGeeks Solution
+
 class Solution {
   public:
     int countAtMostK(vector<int> &arr, int k) {
