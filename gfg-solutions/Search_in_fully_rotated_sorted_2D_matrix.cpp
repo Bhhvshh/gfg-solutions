816b0d9fe60ccd873ee61a563bc93b1bbe057a6a
Bhhvshh <149655632+Bhhvshh@users.noreply.github.com>
2025-08-20 17:03:09 +0530

    Add GFG solution: Search_in_fully_rotated_sorted_2D_matrix (2025-08-20)

---
+++ gfg-solutions/Search_in_fully_rotated_sorted_2D_matrix.cpp
@@ -0,0 +1,25 @@
+// Problem: Search_in_fully_rotated_sorted_2D_matrix
+// Description: You are given a 2D matrix mat[][] of size n x m that was initially filled in the following manner: Each row is sorted in increasing order from left to right.
+The first element of every row is greater than the last element of the previous row. This implies that if the matrix is flattened row-wise, it forms a strictly sorted 1D array.
+Later, this sorted 1D array was rotated at some unknown pivot. The rotated array was then written back into the matrix row-wise to form the current matrix. Given such a matrix mat[][] and an integer x, determine whether x exists in the matrix. Examples: Input: x = 3,
+mat[][] = [[7, 8, 9, 10], [11, 12, 13, 1], [2, 3, 4, 5]] Output: true
+Explanation: 3 is located at the 3rd row and 2nd column.
+Input: x = 10,
+mat[][] = [[6, 7, 8], [9, 1, 2], [3, 4, 5]]
+Output: false
+Explanation: The value 10 does not exist in the matrix.
+Constraint:
+1 ≤ n × m ≤ 105
+1 ≤ mat[i][j], x ≤ 106
+// GeeksforGeeks Solution
+
+class Solution {
+  public:
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        for(auto &z:mat)
+         for(auto y:z) if(y==x) return true;
+         
+         return false;
+    }
+};
